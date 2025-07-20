2419a366768b6d4cb0c9aa03bbeda43c66fd524f
Alena Petrych <abelokonskaya.87@gmail.com>
2025-07-20 09:28:45 +0100

    add task-04-hoisting

--- src/index.js
+++ src/index.js
@@ -1,41 +1,21 @@
-const tasks = [
-  { number: "01", title: "Sum Numbers" },
-  { number: "02", title: "Const" },
-  { number: "03", title: "Let" },
-  { number: "04", title: "Hoisting" },
-  { number: "05", title: "Ternary Operator" },
-  { number: "06", title: "Arrow Functions" },
-  { number: "07", title: "Default Parameters" },
-  { number: "08", title: "Check Function Parameters" },
-  { number: "09", title: "Object Destructuring" },
-  { number: "10", title: "Destructuring and Rest Operator" },
-  { number: "11", title: "Spread Operator" },
-  { number: "12", title: "Copy Array" },
-  { number: "13", title: "Template Literals" },
-  { number: "14", title: "Object Destructuring" },
-  { number: "15", title: "Iterate Over String" },
-  { number: "16", title: "Swap Variable Values" },
-  { number: "17", title: "IIFE (Immediately Invoked Function Expression)" },
-  { number: "18", title: "Classes" },
-  { number: "19", title: "Iterate Over Object" },
-  { number: "20", title: "Sum Positive and Negative Numbers" },
-];
+/* CHALLENGE 4 - Hoisting
 
-const taskList = document.getElementById("task-list");
+Change code to fix first error after the line 15.
+Error after the line 20 should still be generated.
+*/
 
-tasks.forEach((task) => {
-  const li = document.createElement("li");
-  const link = document.createElement("a");
-  link.href = `https://github.com/AlenaB-dev/js-challenges/tree/task-${task.number}`;
-  link.textContent = `Task ${task.number}: ${task.title}`;
-  li.appendChild(link);
-  taskList.appendChild(li);
-});
+"use strict";
 
-// Toggle Theme
-const themeBtn = document.getElementById("theme-toggle");
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  const dark = document.body.classList.contains("dark");
-  themeBtn.textContent = dark ? "☀️ Light Mode" : "🌙 Dark Mode";
-});
+var a = 5,
+  b = 10;
+
+if (b > a) {
+  let c = 2; // first initialization => them action. Before was action   c = a + b + c; and initialization variable c after it.
+  c = a + b + c;
+  console.log(c);
+  // BEFORE: Uncaught ReferenceError: c is not defined
+  // AFTER: 17
+}
+
+console.log(c);
+// Uncaught ReferenceError: c is not defined
