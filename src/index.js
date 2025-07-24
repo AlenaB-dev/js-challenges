4b808762f72744469e6347baa9fcaabb06381c6d
Alena Petrych <abelokonskaya.87@gmail.com>
2025-07-24 13:20:03 +0100

    add task-20 Sum positive and negative numbers.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,30 @@
-const tasks = [
-  { number: "01", title: "Sum Numbers" },
-  { number: "02", title: "Const" },
-  { number: "03", title: "Let" },
-  { number: "04", title: "Hoisting" },
-  { number: "05", title: "Ternary Operator" },
-  { number: "06", title: "Arrow Functions" },
-  { number: "07", title: "Default Parameters" },
-  { number: "08", title: "Check Function Parameters" },
-  { number: "09", title: "Object Destructuring" },
-  { number: "10", title: "Destructuring and Rest Operator" },
-  { number: "11", title: "Spread Operator" },
-  { number: "12", title: "Copy Array" },
-  { number: "13", title: "Template Literals" },
-  { number: "14", title: "Object Destructuring" },
-  { number: "15", title: "Iterate Over String" },
-  { number: "16", title: "Swap Variable Values" },
-  { number: "17", title: "IIFE (Immediately Invoked Function Expression)" },
-  { number: "18", title: "Classes" },
-  { number: "19", title: "Iterate Over Object" },
-  { number: "20", title: "Sum Positive and Negative Numbers" },
-];
+/* CHALLENGE 20 - Sum positive and negative numbers
 
-const taskList = document.getElementById("task-list");
+Create a function sumPlusMinus() that takes array
+and sums separately positive and negative numbers.
 
-tasks.forEach((task) => {
-  const li = document.createElement("li");
-  const link = document.createElement("a");
-  link.href = `https://github.com/AlenaB-dev/js-challenges/tree/task-${task.number}`;
-  link.textContent = `Task ${task.number}: ${task.title}`;
-  li.appendChild(link);
-  taskList.appendChild(li);
-});
+It should return an object like this:
+{
+  plus: 74, // sum of all positive numbers
+  minus: -54 // sum of all negative numbers
+}
+*/
 
-// Toggle Theme
-const themeBtn = document.getElementById("theme-toggle");
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  const dark = document.body.classList.contains("dark");
-  themeBtn.textContent = dark ? "☀️ Light Mode" : "🌙 Dark Mode";
-});
+"use strict";
+
+var nums = [10, -12, 30, -1, -8, 0, 14, -33, 20];
+
+// Write sumPlusMinus() function here
+
+function sumPlusMinus(arr) {
+  return arr.reduce(
+    (acc, elem) => ({
+      plus: elem > 0 ? acc.plus + elem : acc.plus,
+      minus: elem < 0 ? acc.minus + elem : acc.minus,
+    }),
+    { plus: 0, minus: 0 }
+  );
+}
+
+console.log(sumPlusMinus(nums));
+// {plus: 74, minus: -54}
